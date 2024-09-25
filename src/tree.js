4f553d68fc5ddcfa4d94976a92ed21a90d3b6aa7
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2024-09-25 13:47:19 +0700

    Implement buildTree functions and prettyPrint
    prettyPrint is a helper function to visualize
    how the tree looks

--- src/tree.js
+++ src/tree.js
@@ -1,7 +1,7 @@
 const Node = require("./node");
 
 const Tree = (arr) => {
-  const arrProcess = (arr) => {
+  const processArr = (arr) => {
     const sortedArr = arr
       .filter((item, index) => arr.indexOf(item) === index)
       .toSorted((a, b) => {
@@ -14,16 +14,39 @@ const Tree = (arr) => {
         return 0;
       });
 
-    console.log(sortedArr);
+    return sortedArr;
   };
 
-  const buildTree = (arr) => {
-    const sortedArr = arrProcess(arr);
+  const buildTree = (arr, start, end) => {
+    if (start > end) {
+      return null;
+    }
+
+    const mid = parseInt((start + end) / 2);
+    const node = Node(arr[mid]);
+
+    node.left = buildTree(arr, start, mid - 1);
+    node.right = buildTree(arr, mid + 1, end);
+    return node;
   };
 
-  let root = buildTree(arr);
+  const sortedArr = processArr(arr);
+  let root = buildTree(sortedArr, 0, sortedArr.length - 1);
 
-  return {};
+  return root;
 };
 
-module.exports = Tree;
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+module.exports = { Tree, prettyPrint };
