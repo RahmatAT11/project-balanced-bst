4d985e354081b656070d996a7cf0542aaa9fa38d
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2024-10-30 11:22:49 +0700

    Release: v1.0.0

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-const { Tree, prettyPrint } = require('./src/tree');
+const { Tree, prettyPrint } = require("./src/tree");
 
-const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-
-const test = Tree(arr);
-const node = test.insert(test.root, 2);
-
-prettyPrint(test.root, "~", false);
+module.exports = { Tree, prettyPrint };
